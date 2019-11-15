e3fdf8ea47dcc919160f000cf17a5999cf13574a
htandmy <htandmy@qq.com>
2019-11-15 15:03:27 +0900

    ver 0.0.2

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import bunnypng from './bunny.png'
 
 const app = new PIXI.Application();
 var gameBox = document.getElementById("app")
@@ -7,24 +8,12 @@ var gameBox = document.getElementById("app")
 gameBox.appendChild(app.view);
 
 // load the texture we need
-app.loader.add('bunny', 'bunny.png').load((loader, resources) => {
-    // This creates a texture from a 'bunny.png' image
-    const bunny = new PIXI.Sprite(resources.bunny.texture);
-
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
-
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // Add the bunny to the scene we are building
-    app.stage.addChild(bunny);
-
-    // Listen for frame updates
-    app.ticker.add(() => {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
+const bunny = PIXI.Sprite.from(bunnypng);
+bunny.x = app.screen.width / 2;
+bunny.y = app.screen.height / 2;
+bunny.anchor.set(0.5);
+app.stage.addChild(bunny);
+app.ticker.add(() => {
+    // just for fun, let's rotate mr rabbit a little
+    bunny.rotation += 0.1;
 });
