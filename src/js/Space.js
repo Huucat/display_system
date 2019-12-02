fa1939fa81f3c2bed8f0ef9c83a91df39c873969
htandmy <htandmy@qq.com>
2019-12-02 15:56:18 +0900

    update

--- src/js/Space.js
+++ src/js/Space.js
@@ -5,12 +5,12 @@ export default class Space {
     constructor(app){
         this.spaceBox = new PIXI.Container();
 
-        this.spaceBg = new PIXI.Sprite(app.loader.resources['spaceBg_01'].texture);
+        this.spaceBg = new PIXI.Sprite(app.loader.resources['spaceBg_02'].texture);
         this.spaceBg.anchor.set(0.5);
     
         this.spaceBox.addChild(this.spaceBg);
         this.spaceBox.x = app.screen.width / 2;
-        this.spaceBox.y = app.screen.height / 2 + 600;
+        this.spaceBox.y = app.screen.height / 2 + 1000;
         // this.spaceBox.scale.x = this.spaceBox.scale.y = 0.3;
         
         app.stage.addChild(this.spaceBox);
@@ -24,6 +24,6 @@ export default class Space {
     }
 
     rotation(){
-        this.spaceBox.rotation -= 0.001;
+        this.spaceBox.rotation -= 0.0005;
     }
 }
--- src/js/constellation.js
+++ src/js/constellation.js
@@ -1,17 +1,17 @@
-import { OutlineFilter } from '@pixi/filter-outline';
+import { GlowFilter } from '@pixi/filter-glow';
 import * as PIXI from 'pixi.js';
 
 export default class Constellation {
     constructor(app){
         this.constellationBox = new PIXI.Container();
-        this.outLine = new OutlineFilter(2, 0x99ff99);
+        this.outLine = new GlowFilter(15, 2, 0, 0xFF00FF, 0.5);
         this.constellationBoxList = [];
         this.init(app);
     }
 
     init(app){
         let self = this;
-        let n = 20;
+        let n = 30;
 
         for(let i = 0 ; i < n ; i++){
             this.constellationBoxList[i] = new PIXI.Sprite(app.loader.resources['constellation_01'].texture);
@@ -21,7 +21,7 @@ export default class Constellation {
                 this.constellationBoxList[i].rotation = (Math.PI * 2 / n) * i;
             }
             this.constellationBoxList[i].anchor.x = 0.5;
-            this.constellationBoxList[i].anchor.y = 3.5;
+            this.constellationBoxList[i].anchor.y = 5;
             this.constellationBoxList[i].interactive = true;
             this.constellationBoxList[i].id = i;
             this.constellationBoxList[i].scale.x = this.constellationBoxList[i].scale.y = 0.5;
