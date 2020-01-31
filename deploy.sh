28384cc0046c18d6c388641d532e3566e9c9cffd
htandmy <htandmy@qq.com>
2020-01-31 14:40:51 +0900

    ver 0.9.24

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+# 错误时停止
+set -e
+
+# 打包
+npm run build
+
+# 进入目标文件夹
+cd dist
+
+# 提交到本地仓库
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 提交到项目的 gh-pages 分支
+git push -f https://github.com/HTandMY/display_system.git master:gh-pages
+
+cd -
